8d529737ac7e15bfda819eb318cb9ae862d3feea
Andrey Galkin <andrey@futoin.eu>
2014-10-06 16:28:52 +0300

    Added ChannelContext and HTTP/ChannelContext according to FTN5 & 6 changes

---
+++ src/FutoIn/RI/Executor/ChannelContext.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * FutoIn Communication Channel Context - Reference Implementation
+ *
+ * @package FutoIn\Core\PHP\RI\Executor
+ * @copyright 2014 FutoIn Project (http://futoin.org)
+ * @author Andrey Galkin
+ */
+
+namespace FutoIn\RI\Executor;
+
+/**
+ * FutoIn Communication Channel Context
+ *
+ * @see http://specs.futoin.org/final/preview/ftn6_iface_executor_concept-1.html
+ * @api
+ */
+abstract class ChannelContext implements \FutoIn\Executor\ChannelContext
+{
+    use \FutoIn\RI\Details\AsyncStepsStateAccessorTrait;
+
+    private $state;
+    
+    public function __construct()
+    {
+        $this->state = new \StdClass;
+    }
+
+    /**
+     * Get channel state variables
+     * @note state is persistent only for stateful protocols
+     * @return array of key-value pairs
+     */
+    public function state()
+    {
+        return $this->state;
+    }
+    
+    /** @internal */
+    public function __clone()
+    {
+        throw new \FutoIn\Error( \FutoIn\Error::InternalError );
+    }
+}
---
+++ src/FutoIn/RI/Executor/HTTP/ChannelContext.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * FutoIn HTTP Communication Channel Context - Reference Implementation
+ *
+ * @package FutoIn\Core\PHP\RI\Executor
+ * @copyright 2014 FutoIn Project (http://futoin.org)
+ * @author Andrey Galkin
+ */
+
+namespace FutoIn\RI\Executor\HTTP;
+
+/**
+ * FutoIn HTTP Communication Channel Context - Reference Implementation 
+ * for standard PHP process
+ *
+ * @see http://specs.futoin.org/final/preview/ftn5_iface_http_integration-1.html
+ * @api
+ */
+class ChannelContext
+    extends \FutoIn\RI\Executor\ChannelContext
+{
+    protected $is_secure = false;
+    private $tmp_options = null;
+
+    public function __construct( $is_secure )
+    {
+        $this->is_secure = $is_secure;
+    }
+
+    /**
+     * Get type of channel
+     * HTTP
+     * LOCAL
+     * TCP
+     * UDP
+     * any other - as non-standard extension
+     * @return string type identifier
+     */
+    public function type()
+    {
+        return self::TYPE_HTTP;
+    }
+    
+    /**
+     * Check if current communication channel between Invoker and Executor is stateful
+     * @return boolean
+     */
+    public function isStateful()
+    {
+        return false;
+    }
+    
+    /**
+     * Get all request headers as map
+     * @return array of name->value pairs
+     */
+    public function getRequestHeaders()
+    {
+        return getallheaders();
+    }
+    
+    /**
+     * @param $name Header name
+     * @param $value Header value
+     * @param $override Override previous headers with the same name
+     * @return void
+     */
+    public function setResponseHeader( $name, $value, $override=true )
+    {
+        header( "$name: $value", $override );
+    }
+    
+    /**
+     * Set HTTP status code
+     * @param $http_code HTTP status code
+     * @return void
+     */
+    public function setStatusCode( $http_code )
+    {
+        http_response_code( $http_code );
+    }
+    
+    /**
+     * Set cookie value
+     * @param $name Cookie name
+     * @return cookie value or null
+     */
+    public function getCookie( $name )
+    {
+        if ( isset( $_COOKIE[$name] ) )
+        {
+            return $_COOKIE[$name];
+        }
+        
+        return null;
+    }
+    
+    /**
+     * Set cookie
+     * @param $name Cookie name
+     * @param $value Cookie value
+     * @param $options array|object with the following fields:
+     *      options.http_only = true 
+     *      options.secure = INFO_SECURE_CHANNEL
+     *      options.domain = null
+     *      options.path = null
+     *      options.expires = null (date object or string)
+     *      options.max_age = null (interval object or string)
+     * @return void
+     */
+    public function setCookie( $name, $value, $options )
+    {
+        $http_only = true;
+        $secure = $this->is_secure;
+        $domain = null;
+        $path = null;
+        $expires = null;
+        $max_age = null;
+        
+        $this->tmp_options = (array)$options;
+        $extract_count = extract( $this->tmp_options, EXTR_IF_EXISTS|EXTR_REFS );
+        $total_count = count( $this->tmp_options );
+        unset( $this->tmp_options );
+
+        if ( ( $extract_count !== $total_count ) ||
+             ( !is_null( $max_age ) && !is_null( $expires ) ) ||
+             ( !is_null( $max_age ) && !( $max_age instanceof \DateTimeInterface ) ) ||
+             ( !is_null( $expires ) && !( $expires instanceof \DateInterval ) ) )
+        {
+            throw new \FutoIn\Error( \FutoIn\Error::InternalError, "Invalid options for setCookie" );
+        }
+        
+        if( !is_null( $max_age ) )
+        {
+            $expires = new \DateTime;
+            $expires->add( $max_age );
+        }
+        
+        if ( !is_null( $expires ) )
+        {
+            $expires = $expires->getTimestamp();
+        }
+        else
+        {
+             $expires = 0;
+        }
+        
+        setcookie( $name, $value, $expires, $path, $domain, $secure, $http_only );
+    }
+}
